f7b09d938bbc07dc4400ea91c08cd34c18416657
Gallagher <Gallagher@ADMINIB-DKKF3L7.raleigh.ibm.com>
2013-08-27 14:21:37 -0400

    Error in Manifest fixed

--- src/com/sandz/apptorquevehiclemaint/ConfigActivity.java
+++ src/com/sandz/apptorquevehiclemaint/ConfigActivity.java
@@ -1,8 +1,26 @@
 package com.sandz.apptorquevehiclemaint;
 
-import android.os.Bundle;
+import org.prowl.torque.remote.ITorqueService;
+
+import java.text.NumberFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConfigActivity extends Activity {
 
@@ -10,13 +28,6 @@ public class ConfigActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_config);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.config, menu);
-		return true;
 	}
 
 }
